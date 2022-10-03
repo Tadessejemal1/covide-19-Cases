ee3f097e6195ae174a4fe2d1053ed0b0196369d1
Tadesse Jemal <tadessejemal63@gmail.com>
2022-10-03 18:49:20 +0300

    set-up initial stages of the project

---
+++ src/Redux/configureStore.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import reducer from './Redux';
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger, thunk),
+);
+
+export default store;
