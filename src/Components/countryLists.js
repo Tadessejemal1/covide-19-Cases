5956b7a46626308891308a9639ac65238a8be7fc
Tadesse Jemal <tadessejemal63@gmail.com>
2022-10-04 19:15:56 +0300

    styling the project

--- src/Components/countryLists.js
+++ src/Components/countryLists.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { AiOutlineRightCircle } from 'react-icons/ai';
+import { BsArrowRightCircle } from 'react-icons/bs';
 import PropTypes from 'prop-types';
 
 const CountryLists = ({ id, country, totallConfirmed }) => (
   <Link className="countries" to={`details/${id}`} key={id}>
     <div className="country display">
       <h2 className="ea-country">{country.Country}</h2>
-      <AiOutlineRightCircle />
+      <BsArrowRightCircle />
 
     </div>
     {totallConfirmed}
